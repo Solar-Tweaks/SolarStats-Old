891f1c025a4b025cbd603771744a2ef1b5264036
RichardDorian <richard.dorian@outlook.com>
2022-04-24 21:34:15 +0200

    refactor: :recycle: refactor os handlind with systray

--- src/utils/systray.ts
+++ src/utils/systray.ts
@@ -7,17 +7,13 @@ import Logger from '../Classes/Logger';
 export default function setupTray(): SysTray {
   if (process.argv.includes('--noTray')) return null;
 
-  const items: MenuItem[] = [];
-
-  if (process.platform === 'win32')
-    items.push({
-      title: 'Hide/Show window',
+  const items: MenuItem[] = [
+    {
+      title: 'Show window',
       tooltip: 'Hide or show the terminal window',
       checked: false,
-      enabled: true,
-    });
-
-  items.push(
+      enabled: process.platform === 'win32',
+    },
     {
       title: 'Reload config',
       tooltip: 'Reload Solar Stats Config',
@@ -29,13 +25,14 @@ export default function setupTray(): SysTray {
       tooltip: 'Shutdown Solar Stats',
       checked: false,
       enabled: true,
-    }
-  );
+    },
+  ];
 
   const systray = new SysTray({
     menu: {
       title: 'Solar Stats',
-      tooltip: 'Solar Stats Proxy',
+      tooltip:
+        'Click to see Solar Stats tray menu. Take actions easily from here.',
       icon: readFileSync(
         join(__dirname, '..', '..', 'assets', 'icon.ico'),
         'base64'
@@ -46,17 +43,24 @@ export default function setupTray(): SysTray {
   });
 
   systray.onClick(async (event) => {
-    // Can't rely on `seq_id` because the menu depends on the os
-    switch (event.item.title) {
-      case 'Reload config':
+    switch (event.seq_id) {
+      case 0:
+        // When no `visible` argument is passed, the visibility is toggled
+        Logger.warn('Window visibility is not implemented yet');
+        systray.sendAction({
+          type: 'update-item',
+          item: {
+            ...event.item,
+            checked: !event.item.checked,
+          },
+          seq_id: event.seq_id,
+        });
+        break;
+      case 1:
         await reloadConfig();
         Logger.info('Config reloaded');
         break;
-      case 'Hide/Show window':
-        // When no `visible` argument is passed, the visibility is toggled
-        Logger.warn('Window visibility is not implemented yet');
-        break;
-      case 'Exit':
+      case 2:
         systray.kill();
         process.exit(0);
       default:
